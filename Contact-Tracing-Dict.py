6b5cf939aab8d144fa6192adf0c47f11d7499329
Mary Gwen Susa <Mary Gwen G. Susa>
2022-11-12 12:18:36 +0800

    Menu displayed through printing dosctring

---
+++ Contact-Tracing-Dict.py
@@ -0,0 +1,38 @@
+#Write a python program for contact tracing: 
+
+
+#Allow user to select item in the menu (check if valid) 
+def menuOption():
+    #Display a menu of options 
+    """
+    Menu: 
+        1 -> Log-in 
+        2 -> Search 
+        3 -> Exit (y/n)
+    """
+
+print(menuOption.__doc__)
+
+
+"""- Perform the selected option 
+- Option 1: Ask personal data for contact tracing (Listed are sample only, add more) 
+Use dictionary to store the info 
+Use the full name as key 
+The value is another dictionary of personal information 
+- Option 2: Search, ask full name then display the record 
+- Option 3: Ask the user if want to exit or retry. 
+Menu: 1 -> Add an item 2 -> Search 3 -> Exit (y/n) 
+What do you want to do? (1-3): 1 
+Full name: Danilo Madrigalejos 
+Age: 30 
+Address: Eastwood 
+Phone number: 1234567890 
+Saved! 
+What do you want to do? (1-3): 2 
+Full name: Danilo Madrigalejos 
+Age: 30 
+Address: Eastwood 
+Phone number: 1234567890 
+What do you want to do? (1-3): 3 
+Exit? n
+"""
